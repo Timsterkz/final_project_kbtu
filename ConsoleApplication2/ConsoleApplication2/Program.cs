4e009eb815ee7d9a45a596f4dbb9658dce7df16b
Timsterkz <timters5@gmail.com>
2017-03-09 12:55:40 +0600

    Add files via upload

---
+++ ConsoleApplication2/ConsoleApplication2/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace mid1
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            int n = int.Parse(Console.ReadLine()); 
+                for (int i = 1; i <= n; ++i) 
+            {
+                if (n % i == 0)
+                {
+                    Console.Write(i + " ");
+                }
+            }
+        }
+    }
+}
+       
+          
